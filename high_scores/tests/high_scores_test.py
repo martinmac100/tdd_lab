d5ef75c5b131e427b4d9460d89cb4fddd3af0e21
mjmccoach <mmccoach88@gmail.com>
2020-09-17 12:55:44 +0100

    initial commit passed test 1

---
+++ high_scores/tests/high_scores_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+from src.high_scores import latest, personal_best, personal_top_three
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
+
+
+class HighScoresTest(unittest.TestCase):
+    def setUp(self):
+        self.scores = [34, 54, 765, 234, 1, 32, 764, 98, 901, 378, 811]
+
+    def test_latest(self):
+        self.assertEqual(811,latest(self.scores))
+
+    # Tests
+
+    # Test latest score (the last thing in the list)
+
+
+
+    # Test personal best (the highest score in the list)
+
+    # Test top three from list of scores
+
+    # Test ordered from highest tp lowest
+
+    # Test top three when there is a tie
+
+    # Test top three when there are less than three
+
+    # Test top three when there is only one
